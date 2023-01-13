1fc6494bc2953d8b98e1930ba43e6138a7423830
Mufadal03 <sadrimufadal@gmail.com>
2023-01-13 17:20:53 +0530

    initialize homepage

---
+++ client/value-enable/src/Routes/Home.jsx
@@ -0,0 +1,20 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    const { isLoading, isError, isAuth } = useSelector(state => {
+        return {
+            isAuth:state.isAuth
+        }
+    })
+    console.log(isAuth)
+    return (
+        <Box>
+            <Navbar />
+      </Box>
+  )
+}
+
+export default Home
