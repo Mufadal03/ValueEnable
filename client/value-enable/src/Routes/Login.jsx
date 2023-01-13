35002e16bd63707d73054fa4448b605447090ab4
Mufadal03 <sadrimufadal@gmail.com>
2023-01-13 18:28:58 +0530

    added Login Signup

--- client/value-enable/src/Routes/Login.jsx
+++ client/value-enable/src/Routes/Login.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { Box, Button, Flex, Image, Input, Text } from "@chakra-ui/react"
+import {Link} from "react-router-dom"
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { LogIn } from '../redux/authRedux/actions'
 const Login = () => {
+  const [user, setUser] = useState({ email: "", password: "" })
+  const [disable,setDisable] = useState(true)
+  const dispatch = useDispatch()
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUser({
+      ...user,
+      [name]:value
+    })
+  }
+  useEffect(() => {
+  const { password, email } = user
+   if(password!=""&&email!="")setDisable(false)
+  }, [user])
+  
+  const handleSubmit = () => {
+    const { name, email } = user
+    if (name == '' || email == "" ) return
+    console.log(user)
+    dispatch(LogIn(user))
+  }
+
   return (
-    <div>Login</div>
+    <Flex h='100vh' >
+      <Box w='50%' bgImage={`url(https://platform.valuenable.in/assets/images/login-banner.png)`} bgPosition="center" bgSize={'cover'} ></Box>
+      <Box w="50%">
+        <Flex direction={"column"} gap="2rem" px='2rem' border={"1px solid rgba(1,1,1,0.5)"} w="70%" h="500px" m={"5rem auto"}>
+          <Image src='https://platform.valuenable.in/assets/images/logo.png' alt="logo" h="100px" m="1rem auto" />
+          <Input type="email" onChange={handleChange} name="email" placeholder='email' />
+          <Input type="password" onChange={handleChange} name="password" placeholder='password' />
+          <Button disabled={disable} colorScheme={"messenger"} onClick={handleSubmit}>Login</Button>
+          <Link to="/signup"><Text >Don't have an account ? signIn</Text></Link>
+        </Flex>
+      </Box>
+   </Flex>
   )
 }
 
--- client/value-enable/src/Routes/Signup.jsx
+++ client/value-enable/src/Routes/Signup.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { Box, Button, Flex, Image, Input, Text } from "@chakra-ui/react"
+import {Link} from "react-router-dom"
+import { useState } from 'react'
+import {useDispatch} from "react-redux"
+import { signUp } from '../redux/authRedux/actions'
 const Signup = () => {
-  return (
-    <div>Signup</div>
+  const [disable, setDisable] = useState(true)
+  const [userData, setUserData] = useState({
+    email: "",
+    name: "",
+    password:""
+  })
+  const dispatch = useDispatch()
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUserData({
+      ...userData,
+      [name]:value
+    })
+  }
+  useEffect(() => {
+  const { name, email, password } = userData
+   if(name!=""&&email!=""&&password!="")setDisable(false)
+  }, [userData])
+  
+  const handleSubmit = () => {
+    const { name, email, password } = userData
+    if (name == '' || email == "" || password == "") return
+    dispatch(signUp(userData))
+  }
+
+  
+
+    return (
+    <Flex h='100vh' >
+      <Box w='50%' bgImage={`url(https://platform.valuenable.in/assets/images/login-banner.png)`} bgPosition="center" bgSize={'cover'} ></Box>
+      <Box w="50%">
+        <Flex direction={"column"} gap="2rem" px='2rem' border={"1px solid rgba(1,1,1,0.5)"} w="70%" h="500px" m={"5rem auto"}>
+          <Image src='https://platform.valuenable.in/assets/images/logo.png' alt="logo" h="100px" m="1rem auto" />
+          <Input type='text'  name="name"  onChange={handleChange} placeholder="name" />
+          <Input type="email"  name="email"  onChange={handleChange} placeholder='email' />
+          <Input type="password" name="password"  onChange={handleChange} placeholder='password' />
+            <Button disabled={disable} colorScheme={"messenger"} onClick={handleSubmit}>Signup</Button>
+          <Link to="/login"><Text >have an account ? LogIn</Text></Link>
+            
+        </Flex>
+      </Box>
+   </Flex>
   )
 }
 
