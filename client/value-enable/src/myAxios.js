ac56f990ac0d0984b297f505782c8f0125a1fa5f
Mufadal03 <sadrimufadal@gmail.com>
2023-01-14 18:58:02 +0530

    base url localhost

--- client/value-enable/src/myAxios.js
+++ client/value-enable/src/myAxios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL:'http://localhost:4000'
+    baseURL: 'http://localhost:4000'
+    // baseURL:"https://backend-l26n.vercel.app/"
     // baseURL:"https://backend-sigma-three.vercel.app/"
 })
