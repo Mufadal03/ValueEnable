41de4748d73ba637fd606a16a6d8d7ded667724e
Mufadal03 <sadrimufadal@gmail.com>
2023-01-14 16:14:16 +0530

    Illustration route added

--- client/value-enable/src/Routes/AllRoutes.jsx
+++ client/value-enable/src/Routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Req from '../PrivateRoute/Req'
 import Home from './Home'
+import Illustration from './Illustration'
 import Login from './Login'
 import PolicyCalculation from './PolicyCalculation'
 import Signup from './Signup'
@@ -12,7 +13,8 @@ const AllRoutes = () => {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/policy-calculation" element={<Req><PolicyCalculation /></Req>}/>
+            <Route path="/policy-calculation" element={<Req><PolicyCalculation /></Req>} />
+            <Route path='/Illustration' element={<Illustration />} />
        </Routes>
   )
 }
