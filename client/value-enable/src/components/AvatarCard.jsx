365e1d851079ee6d8c4db7281b7ab03c78b3121d
Mufadal03 <sadrimufadal@gmail.com>
2023-01-14 16:48:37 +0530

    AvatarCard updated

--- client/value-enable/src/components/AvatarCard.jsx
+++ client/value-enable/src/components/AvatarCard.jsx
@@ -1,19 +1,28 @@
-import { Avatar, Box, Flex, Heading } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AvatarCard = (props) => {
-  return (
-      <Flex border={"2px solid red"} h="300px" w='80%' m="auto" textAlign={"left"}>
-          <Flex justifyContent={"center"}alignItems="center" border={"2px solid green"} flexGrow="1"> 
+    console.log(props)
+    return (
+      <>
+            <Flex border={"1px solid rgba(1,1,1,.5)"} h="300px" w='80%' m="auto" textAlign={"left"}>
+                
+          <Flex justifyContent={"center"}alignItems="center"flexGrow="1"> 
               <Avatar h="200px" w='200px'/>
           </Flex>
-          <Flex border={"2px solid blue"} flexGrow="3" direction={"column"}>
-              <Heading>Name</Heading>
-              <Heading>Email</Heading>
-              <Heading>Policy term</Heading>
-              <Heading>Policy paying term</Heading>
+          <Flex flexGrow="3" direction={"column"} gap=".5rem" justifyContent={"center"}>
+              <Heading fontFamily="cursive" fontSize={'2xl'}>Welcome {props.name }</Heading>
+              <Heading fontFamily="cursive" fontSize={'2xl'}>{ props.email}</Heading>
+              <Heading fontFamily="cursive" fontSize={'2xl'}>Policy term { props.policy_term} years</Heading>
+              <Heading fontFamily="cursive" fontSize={'2xl'}>Policy paying term {props.premium_paying_term} years</Heading>
+                    <Heading fontFamily="cursive" fontSize={'2xl'}>Choosen frequency {props.premium_frequency}</Heading>
+                    <Heading fontFamily="cursive" fontSize={'2xl'}>{props.premium_frequency} Due { props.modal_premium} Rs</Heading>
+              <Heading fontFamily="cursive" fontSize={'2xl'}>Sum assured {props.sum_assured } Rs</Heading>
           </Flex>
-   </Flex>
+      </Flex>
+           
+      </>
   )
 }
 
