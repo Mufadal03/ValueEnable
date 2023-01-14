7bcbfc52bb61a4d90a63a77b93cc1955168b2a26
Mufadal03 <sadrimufadal@gmail.com>
2023-01-14 14:39:00 +0530

    added routes to the server

--- server/server.js
+++ server/server.js
@@ -3,14 +3,16 @@ const { connection } = require("./config/db")
 const { RegisterationController } = require("./routes/register.routes")
 const app = express() 
 const cors = require("cors")
+const { PolicyController } = require("./routes/policy.routes")
+const { Authentication } = require("./middleware/authenticate")
 app.use(express.json())  
 app.use(cors()) 
-
 
 app.get("/", (req,res) => {
     res.send("homepage")
 })
-app.use("/user",RegisterationController)
+app.use("/user", RegisterationController)
+app.use("/policy",Authentication,PolicyController)
 app.listen(4000, async () => {
     try {
         await connection
